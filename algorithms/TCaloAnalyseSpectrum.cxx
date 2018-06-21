688c215ec3b2329d9719bf080735594137a115b8
redeboer <r.e.deboer@students.uu.nl>
2018-06-21 14:28:43 +0200

    Gaussian fit for calo analyse spectrum

--- algorithms/TCaloAnalyseSpectrum.cxx
+++ algorithms/TCaloAnalyseSpectrum.cxx
@@ -22,10 +22,10 @@
 	// INITIALISE FUNCTION: nothing to initialise
 	void TCaloAnalyseSpectrum::Initialise() {}
 
-	// RUN FUNCTION: nothin to run
+	// RUN FUNCTION: nothing to run
 	StatusCode TCaloAnalyseSpectrum::Run() { return Success; }
 
-	// FINALISE FUNCTION: open TTree from TCaloWriter and analyse it. 
+	// FINALISE FUNCTION: open TTree from TCaloWriter and analyse spectrum using a Gaussian fit
 	void TCaloAnalyseSpectrum::Finalise()
 	{
 		// Get access to the calo output file
@@ -53,76 +53,37 @@
 		TH1I* hist_sum = new TH1I("energy_sum",
 			"Enery spectrum computed with sum",
 			200, 0, 12e5 ); // 0, tree->GetMaximum("energy_sum")
-		//TH1I* hist_lsp = new TH1I("energy_lsp",
-		//	"Enery spectrum computed with LSP function",
-		//	200, 0, 12e5 ); // 0, tree->GetMaximum("energy_lsp")
-		//TH1I* hist_lan = new TH1I("energy_lan",
-		//	"Enery spectrum computed with LSP function",
-		//	200, 0, 12e5 ); // 0, tree->GetMaximum("energy_sum")
 		// Loop over Tree to make histograms
 		for( Long64_t i=0; i<nentries; ++i ) {
 			tree->GetEntry(i);
 			hist_sum->Fill(energy_sum);
-		//	hist_lsp->Fill(energy_lsp);
-		//	hist_lan->Fill(energy_lan);
 		}
-		if(true) { // Fit or no fit?
-			// Fit functions
-			TF1* fit_sum = new TF1("fit_sum","gaus");
-			TF1* fit_lsp = new TF1("fit_lsp","gaus");
-			TF1* fit_lan = new TF1("fit_lan","gaus");
-			// Helpers for parameters
-			Int_t maxbin;
-			Double_t maxloc;
-			TString title;
-			// Set sigma
-			Double_t sigma = 100;
-			// Set parameters for fit over sum energy
-			maxbin = hist_sum->GetMaximumBin();
-			maxloc = hist_sum->GetBinCenter(maxbin);
-			title.Form("Energy sum spectrum. Peak at: %f;energy (a.u.);counts",maxloc);
-			hist_sum->SetTitle(title.Data());
-			fit_sum->SetParameters(
-				hist_sum->GetBinContent(maxbin),
-				maxloc, // location of peak
-				sigma // sigma
-			);
-			// Set parameters for fit over LSP energy
-			//maxbin = hist_lsp->GetMaximumBin();
-			//maxloc = hist_lsp->GetBinCenter(maxbin);
-			//title.Form("Energy LSP spectrum. Peak at: %f;energy (a.u.);counts",maxloc);
-			//hist_lsp->SetTitle(title.Data());
-			//fit_lsp->SetParameters(
-			//	hist_lsp->GetBinContent(maxbin),
-			//	maxloc, // location of peak
-			//	sigma // sigma
-			//);
-			// Set parameters for fit over Landau energy
-			//maxbin = hist_lan->GetMaximumBin();
-			//maxloc = hist_lan->GetBinCenter(maxbin);
-			//title.Form("Energy Landau spectrum. Peak at: %f;energy (a.u.);counts",maxloc);
-			//hist_lan->SetTitle(title.Data());
-			//fit_lan->SetParameters(
-			//	hist_lan->GetBinContent(maxbin),
-			//	maxloc, // location of peak
-			//	sigma // sigma
-			//);
-			// Fit histograms
-			hist_sum->Fit(fit_sum,"QN","",maxloc-sigma,maxloc+sigma);
-			//hist_lsp->Fit(fit_lsp,"QN","",maxloc-sigma,maxloc+sigma);
-			//hist_lan->Fit(fit_lan,"QN","",maxloc-sigma,maxloc+sigma);
-			hist_sum->GetListOfFunctions()->Add(fit_sum);
-			//hist_lsp->GetListOfFunctions()->Add(fit_lsp);
-			//hist_lan->GetListOfFunctions()->Add(fit_sum);
-			// Gain ownership
-			hist_sum->GetListOfFunctions()->SetOwner();
-			//hist_lsp->GetListOfFunctions()->SetOwner();
-			//hist_lan->GetListOfFunctions()->SetOwner();
-		} // end of fit or no fit
-		// Write histograms
+		// Set parameters for fit over sum energy
+		Double_t sigma = 1e5;
+		Int_t maxbin = hist_sum->GetMaximumBin();
+		Double_t maxloc = hist_sum->GetBinCenter(maxbin);
+		// Create fit function and set parameters
+		TF1* fit_sum = new TF1("fit_sum","gaus",maxloc-2*sigma,maxloc+2*sigma);
+		fit_sum->SetParameters(
+			hist_sum->GetBinContent(maxbin),
+			maxloc, // location of peak
+			sigma // sigma
+		);
+		// Fit histogram, add function, and gain ownership
+		hist_sum->Fit(fit_sum,"QN","",maxloc-2*sigma,maxloc+2*sigma);
+		hist_sum->GetListOfFunctions()->Add(fit_sum);
+		hist_sum->GetListOfFunctions()->SetOwner();
+		// Set new title for 
+		name.Form("Energy spectrum computed with sum (peak: %.2e +/- %.2e, valid entries: %.0f/%u);calo event sum (a.u.);counts",
+			fit_sum->GetParameter(1),
+			fit_sum->GetParameter(2),
+			hist_sum->GetEntries(),
+			pTotalFiles );
+		hist_sum->SetTitle(name.Data());
+		hist_sum->SetOption("err");
+		// Write histogram and delete
 		hist_sum->Write();
-		//hist_lsp->Write();
-		//hist_lan->Write();
+		delete hist_sum;
 		// Close input/output ROOT files
 		pCaloOutputFile->Close();
 		pCaloOutputFile = NULL;
